92badbfe4d1bab0db865a7185f90e95d53d374c1
Topjer <c.schmaeche@gmx.de>
2024-12-02 21:39:39 +0100

    Finalize 2024 Day 2

--- src/2024/2/solution.py
+++ src/2024/2/solution.py
@@ -2,17 +2,24 @@ from utilities import load_file, timing_val
 from pathlib import Path
 CURRENT_FOLDER = Path(__file__).parent.resolve()
 
+def levels_are_valid(levels):
+        diffs = [ x-y for x,y in zip(levels[0:-1], levels[1:])]
+        if not all(abs(x) < 4 and abs(x) > 0 for x in diffs):
+            return False
+        if all(x < 0 for x in diffs):
+            return True
+        if all(x > 0 for x in diffs):
+            return True
+
+        return False
+
 @timing_val
 def part_01(task_input: list[str]) -> int:
     result = 0
     for line in task_input:
         levels = [int(number) for number in line.split()]
-        diffs = [ x-y for x,y in zip(levels[0:-1], levels[1:])]
-        if not all(abs(x) < 4 and abs(x) > 0 for x in diffs):
-            continue
-        if all(x < 0 for x in diffs):
-            result += 1
-        if all(x > 0 for x in diffs):
+
+        if levels_are_valid(levels):
             result += 1
     # put logic here
     return result
@@ -22,29 +29,41 @@ def part_01(task_input: list[str]) -> int:
 def part_02(task_input: list[str]) -> int:
     result = 0
     # put logic here
-    result = 0
     
     for line in task_input:
-        faulty_indices = set()
-        levels = [int(number) for number in line.split()]
-        diffs = [ x-y for x,y in zip(levels[0:-1], levels[1:])]
-        exceeded_increases = [faulty_indices.add(ind) for ind, x in enumerate(diffs) if abs(x) > 3 or abs(x) < 0]
-        positive_numbers = [ ind for ind, x in enumerate(diffs) if x>0]
-        negative_numbers = [ ind for ind, x in enumerate(diffs) if x<0]
-        if len(positive_numbers) == 1:
-            faulty_indices.add(positive_numbers.pop())
-
-        if len(negative_numbers) == 1:
-            faulty_indices.add(negative_numbers.pop())
-
-
-        print(faulty_indices)
+        numbers = line.split()
+        levels = [int(number) for number in numbers]
+        if levels_are_valid(levels):
+            result += 1
+        else:
+            for i in range(0, len(numbers)):
+                temp = numbers.copy()
+                del temp[i]
+                temp_levels = [int(number) for number in temp]
+                if levels_are_valid(temp_levels):
+                    result += 1
+                    break
+                
+        # diffs = [ x-y for x,y in zip(levels[0:-1], levels[1:])]
+        # exceeded_increases = [ind for ind, x in enumerate(diffs) if abs(x) > 3 or abs(x) < 0]
+        # print(exceeded_increases)
+        # if exceeded_increases:
+        #     pass 
+        # positive_numbers = [ ind for ind, x in enumerate(diffs) if x>0]
+        # negative_numbers = [ ind for ind, x in enumerate(diffs) if x<0]
+        # if len(positive_numbers) == 1:
+        #     faulty_indices.add(positive_numbers.pop())
+        #
+        # if len(negative_numbers) == 1:
+        #     faulty_indices.add(negative_numbers.pop())
+        #
+        #
     return result
 
 def main():
     result_part1 = part_01(load_file(CURRENT_FOLDER / 'input'))
     print(f"Outcome of part 1 is: {result_part1}.")
-    result_part2 = part_02(load_file(CURRENT_FOLDER / 'tests/input'))
+    result_part2 = part_02(load_file(CURRENT_FOLDER / 'input'))
     print(f"Outcome of part 2 is: {result_part2}.")
 
 if __name__ == '__main__':
