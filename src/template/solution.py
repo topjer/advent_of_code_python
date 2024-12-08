d1a603fc0578906a683ffae5b40bd1852942af11
Topjer <c.schmaeche@gmx.de>
2024-12-08 19:22:20 +0100

    Update template

--- src/template/solution.py
+++ src/template/solution.py
@@ -1,37 +1,42 @@
-from utilities import load_file, timing_val
+from utilities import load_file, load_file_single, timing_val
 from pathlib import Path
 CURRENT_FOLDER = Path(__file__).parent.resolve()
 
 @timing_val
-def part_01(task_input: list[str]) -> int:
+def part_01(task_input) -> int:
     result = 0
     # put logic here
     return result
 
 
 @timing_val
-def part_02(task_input: list[str]) -> int:
+def part_02(task_input) -> int:
     result = 0
     # put logic here
     return result
 
+def parse_input(task_input):
+    return task_input
+
 def main():
-    result_part1 = part_01(load_file(CURRENT_FOLDER / 'input'))
+    task_input = parse_input(load_file(CURRENT_FOLDER / 'tests/input'))
+    # task_input = parse_input(load_file(CURRENT_FOLDER / 'input'))
+    result_part1 = part_01(task_input)
     print(f"Outcome of part 1 is: {result_part1}.")
-    result_part2 = part_02(load_file(CURRENT_FOLDER / 'input'))
+    result_part2 = part_02(task_input)
     print(f"Outcome of part 2 is: {result_part2}.")
 
 if __name__ == '__main__':
     main()
 
 def test_part1():
-    content = load_file(CURRENT_FOLDER / 'tests/input')
+    content = parse_input(load_file(CURRENT_FOLDER / 'tests/input'))
     result = part_01(content)
     # put test result here
     assert result == 0
 
 def test_part2():
-    content = load_file(CURRENT_FOLDER / 'tests/input')
+    content = parse_input(load_file(CURRENT_FOLDER / 'tests/input'))
     result = part_02(content)
     # put test result here
     assert result == 0
