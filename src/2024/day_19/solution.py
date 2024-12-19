eae8a1405a7868f95b47395720dbdd40048f9e68
Topjer <c.schmaeche@gmx.de>
2024-12-19 23:19:53 +0100

    Clean up 2024 day 19

--- src/2024/day_19/solution.py
+++ src/2024/day_19/solution.py
@@ -2,120 +2,69 @@ from utilities import load_file, load_file_single, timing_val
 from pathlib import Path
 CURRENT_FOLDER = Path(__file__).parent.resolve()
 
-# def find_essential_patterns(patterns):
-#     result = set()
-#     patterns.sort(key=len)
-#     print(patterns)
-
 DESIGN_CACHE = dict()
 
 def core(design, patterns):
+    result = 0
     if design in DESIGN_CACHE:
         return DESIGN_CACHE[design]
-    # print(design)
     if design in patterns:
-        DESIGN_CACHE[design] = True
-        return True
+        result += 1
 
     for i in range(len(design)):
         head = design[:i]
         tail = design[i:]
         if head in patterns:
-            if core(tail, patterns):
-                DESIGN_CACHE[design] = True
-                return True
-    DESIGN_CACHE[design] = False
-    return False
+            result += core(tail, patterns)
 
-def is_reproducible(design, patterns):
-    # print(patterns)
-    # print("in reproducible")
-    # for i in range(1,10):
-    for i in range(1,len(design)-1):
-        # print("foo:", design[-i:])
-        # print()
-        foo = core(design[-i:], patterns)
-        # print(foo)
-        print(DESIGN_CACHE)
+    DESIGN_CACHE[design] = result
+    return result
+
+def get_all_options(design, patterns):
+    """ Core idea is that we start "from the end"
+
+    This way we ensure that we have to compute every combination only once and thus 
+    we get the maximum out of caching.
+    """
+    for i in range(1,len(design)):
+        _ = core(design[-i:], patterns)
     result = core(design, patterns)
-    # print(result)
-    # result = 0
     return result
 
 
-def part_01(task_input) -> int:
-    patterns, designs, max_length = task_input
-    # essential_patterns = find_essential_patterns(patterns)
-    # print(patterns)
-    # print(designs)
-    # print(max_length)
-    result = 0
-    # designs = ['brbwrrruwrrrubrwuugrbuuwuuwrwrbrrgububwurugbwwrb']
-    # designs = ['brgr']
+def part_02(task_input) -> tuple[int, int]:
+    patterns, designs = task_input
+    result_p1 = 0
+    result_p2 = 0
     for design in list(designs):
-        # print(design)
-        if is_reproducible(design, patterns):
-        # if is_reproducible(design, patterns, max_length):
-            result += 1
-            # print('yes')
-        else:
-            print(design)
-        # break
+        res = get_all_options(design, patterns)
+        result_p2 += res
+        if res > 0:
+            result_p1 += 1
 
-    # put logic here
-    return result
-
-
-def part_02(task_input) -> int:
-    patterns, designs, max_length = task_input
-    # essential_patterns = find_essential_patterns(patterns)
-    # print(patterns)
-    # print(designs)
-    # print(max_length)
-    result = 0
-    # designs = ['brbwrrruwrrrubrwuugrbuuwuuwrwrbrrgububwurugbwwrb']
-    # designs = ['brgr']
-    for design in list(designs):
-        # print(design)
-        if is_reproducible(design, patterns):
-        # if is_reproducible(design, patterns, max_length):
-            result += 1
-            # print('yes')
-        else:
-            print(design)
-        # break
-
-    # put logic here
-    return result
+    return result_p1, result_p2
 
 def parse_input(task_input):
     patterns, designs = task_input.split('\n\n')
     patterns = set(patterns.strip().split(', '))
-    max_length = max(len(pat) for pat in patterns)
     designs = designs.strip().split()
-    return patterns, designs, max_length
+    return patterns, designs
 
 @timing_val
 def main():
-    task_input = parse_input(load_file_single(CURRENT_FOLDER / 'tests/test_input'))
+    # task_input = parse_input(load_file_single(CURRENT_FOLDER / 'tests/test_input'))
     print("start")
-    # task_input = parse_input(load_file_single(CURRENT_FOLDER / 'input'))
-    # result_part1 = part_01(task_input)
-    # print(f"Outcome of part 1 is: {result_part1}.")
-    result_part2 = part_02(task_input)
+    task_input = parse_input(load_file_single(CURRENT_FOLDER / 'input'))
+    result_part1, result_part2 = part_02(task_input)
+    print(f"Outcome of part 1 is: {result_part1}.")
     print(f"Outcome of part 2 is: {result_part2}.")
 
 if __name__ == '__main__':
     main()
 
-def test_part1():
-    content = parse_input(load_file_single(CURRENT_FOLDER / 'tests/test_input'))
-    result = part_01(content)
-    # put test result here
-    assert result == 6
-
 def test_part2():
     content = parse_input(load_file_single(CURRENT_FOLDER / 'tests/test_input'))
-    result = part_02(content)
+    result_p1, result_p2 = part_02(content)
     # put test result here
-    assert result == 0
+    assert result_p1 == 6
+    assert result_p2 == 16
